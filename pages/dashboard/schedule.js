7706fbd08a14763072325bc318f4c74b343b6a10
mroger25 <madsonmath@hotmail.com>
2024-02-26 15:26:50 -0300

    Update schedule.js

---
+++ pages/dashboard/schedule.js
@@ -0,0 +1,22 @@
+import React from "react";
+import Head from "next/head";
+
+const Schedule = () => {
+  return (
+    <div>
+      <Head>
+        <title>Ver Escala - Seu Aplicativo de Escalas</title>
+        <meta
+          name="description"
+          content="Ver Escala no Seu Aplicativo de Escalas"
+        />
+      </Head>
+      <main>
+        <h1>Ver Escala</h1>
+        {/* Aqui você pode renderizar a escala do usuário ou qualquer outra informação relacionada */}
+      </main>
+    </div>
+  );
+};
+
+export default Schedule;
